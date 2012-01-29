9101d790526c49ecf4699edfe19911dbbdf83717
Gary Doran, Jr <garydoranjr@gmail.com>
2012-01-29 00:07:47 -0500

    Revise comments

--- asm_syscall.s
+++ asm_syscall.s
@@ -6,13 +6,13 @@ msg_len = . - message
 main:
 
     # Print Message
-    mov    $4, %rax          # write() call = 4
-    mov    $1, %rbx          # STDOUT: file descriptor #1
+    mov    $4, %rax          # write() call: 4
+    mov    $1, %rbx          # file descriptor: 1 (STDOUT)
     mov    $message, %rcx    # Pointer to message
     mov    $msg_len, %rdx    # Length of message
     int    $0x80             # Kernel handles interrupt, performs syscall
 
     # Exit
-    mov    $1, %rax          # exit() call
-    mov    $0, %rbx          # status = 0 (EXIT_SUCCESS)
+    mov    $1, %rax          # exit() call: 1
+    mov    $0, %rbx          # status: 0 (EXIT_SUCCESS)
     int    $0x80             # Kernel handles interrupt, performs syscall
