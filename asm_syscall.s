e3c0a0490bf491b2f234780407be142a17a28811
Gary Doran, Jr <garydoranjr@gmail.com>
2012-01-29 00:05:56 -0500

    Remove quad word instructions

--- asm_syscall.s
+++ asm_syscall.s
@@ -6,13 +6,13 @@ msg_len = . - message
 main:
 
     # Print Message
-    movq    $4, %rax          # write() call = 4
-    movq    $1, %rbx          # STDOUT: file descriptor #1
-    movq    $message, %rcx    # Pointer to message
-    movq    $msg_len, %rdx    # Length of message
-    int     $0x80             # Kernel handles interrupt, performs syscall
+    mov    $4, %rax          # write() call = 4
+    mov    $1, %rbx          # STDOUT: file descriptor #1
+    mov    $message, %rcx    # Pointer to message
+    mov    $msg_len, %rdx    # Length of message
+    int    $0x80             # Kernel handles interrupt, performs syscall
 
     # Exit
-    movq    $1, %rax          # exit() call
-    movq    $0, %rbx          # status = 0 (EXIT_SUCCESS)
-    int     $0x80             # Kernel handles interrupt, performs syscall
+    mov    $1, %rax          # exit() call
+    mov    $0, %rbx          # status = 0 (EXIT_SUCCESS)
+    int    $0x80             # Kernel handles interrupt, performs syscall
