ec7e73f3ce3f52f544b12aabb9bd1691bf951b6a
Gary Doran, Jr <garydoranjr@gmail.com>
2012-01-28 23:57:48 -0500

    Leave space for null termination character

--- write_syscall.c
+++ write_syscall.c
@@ -1,7 +1,7 @@
 #include<unistd.h>
 #include<stdlib.h>
 
-const char message[20] = "Hello System Calls!\n";
+const char message[21] = "Hello System Calls!\n";
 const int msg_len = sizeof(message);
 
 int main() {
